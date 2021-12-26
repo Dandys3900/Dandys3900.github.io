eb6d2c474e157790ff66807c39db202bf3d78422
Tomas Daniel <tomdaniel@email.cz>
2021-12-26 21:31:29 +0100

    Another front-end

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log("app.js is loaded successfully");
+import Web3 from "web3";
+
+App = {
+    web3Provider: null, //At the begining
+
+    init: function() {
+        console.log("App is initialized");
+        return App.initWeb3();
+    },
+
+    initWeb3: function() {
+        if (typeof Web3 !== "undefined") {
+            //If a web3 instance is already running and is provided by Meta Mask
+            App.web3Provider = web3.currentProvider;
+            web3 = new Web3(web3.currentProvider);
+        }
+        else {
+            //Specify default instance if no web3 instance is provided, if connection is not established with Meta Mask use local Ganache insted
+            App.web3Provider = new Web3.providers.HttpProvider("http://localhost:7545");
+            web3 = new Web3(App.web3Provider);
+        }
+    }
+}
+
+window.onload = function() {
+    App.init();
+}
