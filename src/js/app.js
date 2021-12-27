05943a5e7ac1be93d3ade42dce281e30eb25d199
Tomas Daniel <tomdaniel@email.cz>
2021-12-27 22:23:42 +0100

    Web page data displaying, not completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 var App = {
     web3Provider: null, //At the begining
     contracts: {}, //Keep track of all our contracts
+    account: "0x0", //Default null address, empty
+    loading: false,
+    tokenPrice: 1000000000000000,
 
     init: function() {
         console.log("App is initialized");
@@ -9,16 +12,15 @@ var App = {
 
     initWeb3: function() {
         if (typeof Web3 !== "undefined") {
-            //If a web3 instance is already running and is provided by Meta Mask
-            App.web3Provider = ethereum.currentProvider; //původně tam bylo web3.currentProvider
-            web3 = new Web3(ethereum.currentProvider);
+            ethereum.enable().then(() => {
+                web3 = new Web3(web3.currentProvider);
+             });
         }
         else {
-            //Specify default instance if no web3 instance is provided, if connection is not established with Meta Mask use local Ganache insted
-            App.web3Provider = new Web3.providers.HttpProvider("http://localhost:7545");
-            web3 = new Web3(App.web3Provider);
+            web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
         }
 
+        App.web3Provider=web3.currentProvider;
         return App.initContracts();
     },
 
@@ -31,7 +33,53 @@ var App = {
             App.contracts.DandysTokenSale.deployed().then(function(dandysTokenSale) {
                 console.log("Dandys Token Sale Address: ", dandysTokenSale.address);
             });
+        }).done(function() { //We also need to connect DandysToken not only DandysTokenSale
+            $.getJSON("DandysToken.json", function(dandysToken) {
+                App.contracts.DandysToken = TruffleContract(dandysToken);
+                App.contracts.DandysToken.setProvider(App.web3Provider);
+                App.contracts.DandysToken.deployed().then(function(dandysToken) {
+                    console.log("Dandys Token Address: ", dandysToken.address);
+                });
+
+                return App.render(); //Show data
+            });
         });
+    },
+
+    //Function for displaying all stuff on the page, we will switch between showing data and loading animation
+    render: function() {
+        if (App.loading) { //When loading we dont want to display any other data
+            return;
+        }
+        App.loading = true;
+
+        var loader = $("#loader"); //the names came from html file, we want to keep track of these divs
+        var content = $("#content");
+
+        loader.show(); //if loading is true, we want to see loading animation and hide all the data
+        content.hide();
+
+        //Load account data
+        web3.eth.getCoinbase(function(err, account) {
+            if (err === null) {
+                App.account = account;
+                $("#accountAddress").html("Your account address is " + "<b>" + account + "</b>");
+            }
+        });
+
+        var dandysTokenSaleInstance;
+
+        App.contracts.DandysTokenSale.deployed().then(function(instance) {
+            dandysTokenSaleInstance = instance;
+            return dandysTokenSaleInstance.tokenPrice();
+        }).then(function(tokenPrice) {
+            App.tokenPrice = tokenPrice;
+            $(".token-price").html(web3.utils.fromWei(App.tokenPrice, "ether")); //span class is with . instead of #
+        });
+
+        App.loading = false;
+        loader.hide();
+        content.show();
     }
 }
 
